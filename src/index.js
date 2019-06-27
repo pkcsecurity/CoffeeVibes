672996da5055a0061a5a16b168311a3ff7b7279b
Arte Ebrahimi <arteebrahimi@gmail.com>
2019-06-27 16:37:12 -0700

    prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,78 +1,75 @@
 AFRAME.registerComponent('shoot-tomato', {
-  init: function () {
-    const camera = document.getElementById('camera')
+  init: function() {
+    const camera = document.getElementById('camera');
     // const splatSnd = document.querySelector('#splat').components.sound
 
-    this.el.sceneEl.addEventListener('touchstart', event => {
-
+    this.el.sceneEl.addEventListener('touchstart', (event) => {
       // Create element to be thrown, setting position, scale, and model
-      const tomato = document.createElement('a-entity')
-      tomato.setAttribute('position', camera.object3D.position)
-      tomato.setAttribute('scale', '0.2 0.2 0.2')
-      tomato.setAttribute('gltf-model', '#starbucks-glb')
+      const tomato = document.createElement('a-entity');
+      tomato.setAttribute('position', camera.object3D.position);
+      tomato.setAttribute('scale', '0.2 0.2 0.2');
+      tomato.setAttribute('gltf-model', '#starbucks-glb');
 
       // Choose a random rotation offset for some variation
-      const randomRotation = { x: -90 + Math.random() * 30, y: Math.random() * 360, z: 0 }
-      tomato.setAttribute('rotation', randomRotation)
+      const randomRotation = { x: -90 + Math.random() * 30, y: Math.random() * 360, z: 0 };
+      tomato.setAttribute('rotation', randomRotation);
 
       // Set velocity, rotated with camera direction
-      const velocity = new THREE.Vector3(0, 0, -20)
-      velocity.applyQuaternion(camera.object3D.quaternion)
-      tomato.setAttribute('velocity', velocity)
+      const velocity = new THREE.Vector3(0, 0, -20);
+      velocity.applyQuaternion(camera.object3D.quaternion);
+      tomato.setAttribute('velocity', velocity);
 
       // Add physics body
       tomato.setAttribute('body', {
         type: 'dynamic',
         sphereRadius: 0.5,
-        shape: 'sphere'
-      })
+        shape: 'sphere',
+      });
 
       // Add tomato to scene
-      this.el.sceneEl.appendChild(tomato)
+      this.el.sceneEl.appendChild(tomato);
 
-      let didCollide = false
-      tomato.addEventListener('collide', event => {
+      let didCollide = false;
+      tomato.addEventListener('collide', (event) => {
         // Only want to do the splat once, and with the ground only
         if (didCollide || event.detail.body.el.id != 'ground') {
-          return
+          return;
         }
-        didCollide = true
+        didCollide = true;
 
-        tomato.setAttribute('visible', 'false')
+        tomato.setAttribute('visible', 'false');
 
-        const collisionStats = event.detail.contact
+        const collisionStats = event.detail.contact;
 
-        const newElement = document.createElement('a-entity')
+        const newElement = document.createElement('a-entity');
 
-        newElement.setAttribute('position', collisionStats.rj)
+        newElement.setAttribute('position', collisionStats.rj);
 
-        const randomYRotation = Math.random() * 360
-        newElement.setAttribute('rotation', '0 ' + randomYRotation + ' 0')
+        const randomYRotation = Math.random() * 360;
+        newElement.setAttribute('rotation', '0 ' + randomYRotation + ' 0');
 
-        newElement.setAttribute('visible', 'false')
-        newElement.setAttribute('scale', '0.10 0.10 0.10')
+        newElement.setAttribute('visible', 'false');
+        newElement.setAttribute('scale', '0.10 0.10 0.10');
 
-        newElement.setAttribute('gltf-model', '#starbucks-glb')
-        this.el.sceneEl.appendChild(newElement)
+        newElement.setAttribute('gltf-model', '#starbucks-glb');
+        this.el.sceneEl.appendChild(newElement);
 
         newElement.addEventListener('model-loaded', () => {
           // Once the model is loaded, we are ready to show it popping in using an animation
-          newElement.setAttribute('visible', 'true')
+          newElement.setAttribute('visible', 'true');
           newElement.setAttribute('animation', {
             property: 'scale',
             to: '0.15 0.15 0.15',
             easing: 'easeOutElastic',
             dur: 800,
-          })
-        })
+          });
+        });
 
         // We can't remove the thrown tomato until the physics step is over
         setTimeout(() => {
-          tomato.parentNode.removeChild(tomato)
-        }, 0)
-
-
-      })
-    })
-  }
-})
+          tomato.parentNode.removeChild(tomato);
+        }, 0);
+      });
+    });
+  },
+});
